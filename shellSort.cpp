0b436548848ca1459048c2ff0ecf808d980f67b8
MoralesSantiago2 <santiago2004444@gmail.com>
2025-03-17 00:50:20 -0500

    Update shellSort.cpp
    Faltó temporizador al hacer el ordenamiento

--- shellSort.cpp
+++ shellSort.cpp
@@ -1,17 +1,18 @@
 #include "funcionesGlobales.h"
 #include "shellsort.h"
-using namespace std;
-void shellSort(int arr[], int n) {
-   
-    int gap = n / 2;
+#include <iostream>
+#include <sys/timeb.h> 
 
+using namespace std;
+
+void ordenarShell(int arr[], int n) {
+    int gap = n / 2;
     
     while (gap > 0) {
         for (int i = gap; i < n; i++) {
             int temp = arr[i]; 
             int j = i;
 
-           
             while (j >= gap && arr[j - gap] > temp) {
                 arr[j] = arr[j - gap];
                 j -= gap;
@@ -24,34 +25,35 @@ void shellSort(int arr[], int n) {
     }
 }
 
-int callShell() {
+int ejecutarShell() {
     int n;
     cout << "Ingrese la cantidad de numeros: ";
     cin >> n;
 
-    
-    int* arr = new int[n];
+    int arr[n];
 
-    
     cout << "Ingrese los numeros, uno por uno:" << endl;
     for (int i = 0; i < n; i++) {
         cout << "Numero " << i + 1 << ": ";
         cin >> arr[i];
     }
 
-   
     cout << "\nArreglo original: ";
-    printArray(arr, n);
+    mostrarArray(arr, n);
 
-    
-    shellSort(arr, n);
+    struct timeb inicio, fin;
+    ftime(&inicio);  
 
-    
+    ordenarShell(arr, n);
+
+    ftime(&fin);  
+    int tiempoTotal = (int)(1000 * (fin.time - inicio.time) + (fin.millitm - inicio.millitm));
+
     cout << "Arreglo ordenado: ";
-    printArray(arr, n);
+    mostrarArray(arr, n);
 
-    
-    delete[] arr;
+    cout << "Tiempo de ejecución: " << tiempoTotal << " ms" << endl;
 
     return 0;
 }
+
