ae7ab2120593befb73c81a9463ebaa836787e0e9
MoralesSantiago2 <santiago2004444@gmail.com>
2025-03-17 01:21:56 -0500

    Update selectionSort.cpp
    Falta el return al no agregar un archivo txt

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,129 +1,90 @@
 #include "selectionSort.h"
 #include "funcionesGlobales.h"
+#include <iostream>
+#include <fstream>
+#include <chrono>
 
 using namespace std;
 using namespace std::chrono;
 
-void swap(int *a, int *b);
-void selection_sort();
-void selectionSort(int array[], int size);
-
-void swap(int *a, int *b){
+void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-void selectionSort(int array[], int size){
-   for (int step = 0; step < size - 1; step++) {
-    int min_idx = step;
-   for (int i = step + 1; i < size; i++) {
-
-
-      if (array[i] < array[min_idx])
-        min_idx = i;
-   }
-
-    swap(&array[min_idx], &array[step]);
-    cout << "--- Antes del swap" << step + 1 << ": ";
-    printArray(array, size);
-  }
+void selectionSort(int array[], int size) {
+    for (int step = 0; step < size - 1; step++) {
+        int min_idx = step;
+        for (int i = step + 1; i < size; i++) {
+            if (array[i] < array[min_idx])
+                min_idx = i;
+        }
+        swap(&array[min_idx], &array[step]);
+        cout << "--- Antes del swap " << step + 1 << ": ";
+        printArray(array, size);
+    }
 }
 
-void selection_sort(){
-   cout << "--- ingrese opcion 1 para Array quemado.\n";
-   cout << "--- ingrese opcion 2 para Array teclado.\n";
-   cout << "--- ingrese opcion 3 para Array texto.\n";
-   int mode;
-   cin >>mode;
-   if (mode==1){
-    int data[] = {20, 12, 10, 15, 2};
-    int size = sizeof(data) / sizeof(data[0]);
-    cout << "--- El Array original fue: \n";   
-    printArray(data, size);
-    cout << "--- El ordenamiento que se realiza es:\n";
+void selection_sort() {
+    while (true) {
+        cout << "--- Ingrese opcion 1 para Array quemado.\n";
+        cout << "--- Ingrese opcion 2 para Array teclado.\n";
+        cout << "--- Ingrese opcion 3 para Array texto.\n";
+        cout << "--- Ingrese opcion 4 para salir.\n";
+        int mode;
+        cin >> mode;
 
-    auto start = high_resolution_clock::now();
-    selectionSort(data, size);
-    cout << "--- El Array ordenado es:\n";
+        int *data = nullptr;
+        int size = 0;
 
-    auto stop = high_resolution_clock::now();
+        if (mode == 1) {
+            int tempData[] = {20, 12, 10, 15, 2};
+            size = sizeof(tempData) / sizeof(tempData[0]);
+            data = new int[size];
+            copy(tempData, tempData + size, data);
+        } else if (mode == 2) {
+            cout << "--- Ingrese el tamaño del Array:\n";
+            cin >> size;
+            data = new int[size];
+            cout << "--- Ingrese los números del array:\n";
+            for (int i = 0; i < size; i++) {
+                cin >> data[i];
+            }
+        } else if (mode == 3) {
+            ifstream inputFile("array.txt");
+            if (!inputFile.is_open()) {
+                cout << "--- Error: No se pudo abrir el archivo.\n";
+                continue;
+            }
+            inputFile >> size;
+            data = new int[size];
+            for (int i = 0; i < size; i++) {
+                inputFile >> data[i];
+            }
+            inputFile.close();
+        } else if (mode == 4) {
+            cout << "--- Saliendo del programa.\n";
+            break;
+        } else {
+            cout << "--- Opción inválida, intente nuevamente.\n";
+            continue;
+        }
 
-    auto duration = duration_cast<microseconds>(stop - start);
-    printArray(data, size);
+        cout << "--- El Array original fue: \n";
+        printArray(data, size);
+        cout << "--- El ordenamiento que se realiza es:\n";
 
-    cout << "--- El tiempo que demoro hacer el proceso es: "<< duration.count() << " microseconds" << endl;
-   }
-      else{
-   if(mode==2){
-    cout << "--- Bienvenido en esta opción llenamos el array por teclado.\n";
-    int size;
+        auto start = high_resolution_clock::now();
+        selectionSort(data, size);
+        auto stop = high_resolution_clock::now();
 
-    cout << "--- Ingrese el tamaño del Array:\n";
-    cin >> size;
+        cout << "--- El Array ordenado es:\n";
+        printArray(data, size);
 
-    int data[size];
+        auto duration = duration_cast<microseconds>(stop - start);
+        cout << "--- El tiempo que demoró hacer el proceso es: " << duration.count() << " microsegundos" << endl;
 
-    cout << "--- Ingrese los números del array:\n";
-   for (int i = 0; i < size; i++) {
-    cin >> data[i];
-   }
-    cout << "--- El Array original fue: \n";   
-    printArray(data, size);
-    cout << "--- El ordenamiento que se realiza es:\n";
-
-    auto start = high_resolution_clock::now();
-    selectionSort(data, size);
-    cout << "--- El Array ordenado es:\n";
-
-    auto stop = high_resolution_clock::now();
-
-    auto duration = duration_cast<microseconds>(stop - start);
-    printArray(data, size);
-
-    cout << "--- El tiempo que demoro hacer el proceso es: "<< duration.count() << " microseconds" << endl;
-   }
-      else{
-   if(mode==3){
-    cout << "--- Bienvenido en esta opción llenamos el array por documento.\n";
-
-    ifstream inputFile("array.txt");
-
-     if (!inputFile.is_open()) {
-    cout << "--- Error: No se pudo abrir el archivo.\n"<< endl;
-
-    ;
-     }
-    int size;
-
-    inputFile >> size;
-
-    int data[size];
-
-     for (int i = 0; i < size; i++) {
-    inputFile >> data[i];
-     }
-
-    inputFile.close();
-    cout << "--- El Array original fue: \n";   
-    printArray(data, size);
-    cout << "--- El ordenamiento que se realiza es:\n";
-
-    auto start = high_resolution_clock::now();
-    selectionSort(data, size);
-    cout << "--- El Array ordenado es:\n";
-
-    auto stop = high_resolution_clock::now();
-
-    auto duration = duration_cast<microseconds>(stop - start);
-    printArray(data, size);
-
-    cout << "--- El tiempo que demoro hacer el proceso es: "<< duration.count() << " microseconds" << endl;
+        delete[] data;
     }
-      else{
-    cout << "--- fuera de rango.\n";
-    selection_sort();
-         }
-      }
-      }
 }
